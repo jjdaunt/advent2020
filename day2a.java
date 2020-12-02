0c5f64a4df871d997f90a386bbc12a52cb137e98
Jim Daunt <jjdaunt@gmail.com>
2020-12-02 15:45:44 -0500

    Create day2a.java

---
+++ day2a.java
@@ -0,0 +1,34 @@
+package com.scratch;
+
+import static java.lang.Integer.parseInt;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new FileReader("C:\\dev\\scratch\\advent\\input2.txt"))) {
+            System.out.println(br.lines().map(Main::isValid).filter(b -> b).count() + " passwords are valid.");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static boolean isValid(String line) {
+        String[] parts = line.split(" ");
+        String[] minmax = parts[0].split("-");
+        int min = parseInt(minmax[0]);
+        int max = parseInt(minmax[1]);
+        char letter = parts[1].charAt(0);
+        String password = parts[2];
+
+        int count = 0;
+        for (int i = 0; i < password.length(); i++) {
+            if (password.charAt(i) == letter) {
+                count++;
+            }
+        }
+
+        return count <= max && count >= min;
+    }
+}
